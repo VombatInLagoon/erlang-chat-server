c59ce7ac8d1e0f43db116c2069f33941ba7f1fb1
Amin Khorsandiaghai <aminkhorsandi@gmail.com>
2015-04-26 22:08:27 +0200

    Some minor code refactoring.

--- src/controller.erl
+++ src/controller.erl
@@ -1,44 +1,49 @@
 -module(controller).
 -behaviour(gen_server).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, 
+         handle_call/3, 
+         handle_cast/2, 
+         handle_info/2, 
+         terminate/2,
+         code_change/3]).
+
 -export([start/0]).
 
 start() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-% This is called when a connection is made to the server
 init([]) ->
-    Users = dict:new(), % It maps nick to socket
+    % dictionary is used to keep track of online users
+    % and their associated sockets.
+    Users = dict:new(),
     {ok, Users}.
 
-% handle_call is invoked in response to gen_server:call
 handle_call({check_nick, Nick, Socket}, _From, Users) ->
     Response = case dict:is_key(Nick, Users) of
-        true ->
-            NewUsers = Users,
-            nick_in_use;
-        false ->
-            NewUsers = dict:append(Nick, Socket, Users),
-            {ok, user_list(NewUsers)}
-    end,
+                   true ->
+                       NewUsers = Users,
+                       nick_in_use;
+                   false ->
+                       NewUsers = dict:append(Nick, Socket, Users),
+                       {ok, user_list(NewUsers)}
+               end,
     {reply, Response, NewUsers};
 
 handle_call({disconnect, Nick}, _From, Users) ->
     Response = case dict:is_key(Nick, Users) of
-        true ->
-            NewUsers = dict:erase(Nick, Users),
-            ok;
-        false ->
-            NewUsers = Users,
-            user_not_found
-    end,
+                   true ->
+                       NewUsers = dict:erase(Nick, Users),
+                       ok;
+                   false ->
+                       NewUsers = Users,
+                       user_not_found
+               end,
     {reply, Response, NewUsers};
 
 handle_call(_Message, _From, State) ->
     {reply, error, State}.
 
-% handle_cast is invoked in response to gen_server:cast
 handle_cast({say, Nick, Msg}, Users) ->
     broadcast(Nick, Msg, Users),
     {noreply, Users}; 
@@ -62,8 +67,16 @@ handle_cast({left, Nick}, Users) ->
 handle_cast(_Message, State) ->
     {noreply, State}.
 
+handle_info(_Message, State) -> 
+    {noreply, State}.
 
-% auxiliary functions
+terminate(_Reason, _State) -> 
+    ok.
+
+code_change(_OldVersion, State, _Extra) -> 
+    {ok, State}.
+ 
+%% Internal functions
 broadcast(Nick, Msg, Users) ->
     FormatMsg = format_message(Nick, Msg),
     Sockets = [Value || {_, [Value|_]} <- dict:to_list(dict:erase(Nick, Users))],
@@ -78,8 +91,8 @@ nick_list(Nick, Users) ->
         {ok, [Socket|_]} ->
             Nicks = user_list(dict:erase(Nick, Users)),
             gen_tcp:send(Socket, "Online people: " ++ Nicks ++ "\n");
-          _ -> ok    
-     end.
+        _ -> ok    
+    end.
 
 private_message(Recv, Nick, Msg, Users) ->
     FormatMsg = format_message(Nick, Msg),
@@ -99,7 +112,3 @@ format_time() ->
     {H, M, S} = time(),
     io_lib:format('(~2..0b:~2..0b:~2..0b)', [H, M, S]).
 
-% Definitions to avoid gen_server compile warnings
-handle_info(_Message, State) -> {noreply, State}.
-terminate(_Reason, _State) -> ok.
-code_change(_OldVersion, State, _Extra) -> {ok, State}.
--- src/server.erl
+++ src/server.erl
@@ -6,8 +6,12 @@
                 socket}). % the current socket
 
 -export([start_link/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         code_change/3, terminate/2]).
+-export([init/1, 
+         handle_call/3, 
+         handle_cast/2, 
+         handle_info/2,
+         code_change/3, 
+         terminate/2]).
 
 start_link(Socket) ->
     gen_server:start_link(?MODULE, Socket, []).
@@ -28,21 +32,23 @@ handle_cast(accept, S = #state{socket=ListenSocket}) ->
     {noreply, S#state{socket=AcceptSocket, next=nick}};
 
 handle_cast({set_nick, Socket, Nick}, S) ->
-    case gen_server:call(controller, {check_nick, Nick, Socket}) of 
+    Response = gen_server:call(controller, {check_nick, Nick, Socket}),
+    case Response of 
         nick_in_use -> 
             send(Socket, "Nick in use! Pick something else.", []),
             {noreply, S#state{socket=Socket, next=nick}};
         {ok, _} ->
             io:format("~p Joined chat! ~n", [Nick]),
             send(Socket, "Your nick name is ~p. ~n"
-                         "You can always get some help using !h ~n", [Nick]),
+                         "You can always get some help using !h ~n"
+                       , [Nick]),
             gen_server:cast(controller, {join, Nick}),
             {noreply, S#state{socket=Socket, name=Nick, next=chat}}
     end;
 
 handle_cast({private_msg, Socket, Nick, Recv, Msg}, S) ->
     gen_server:cast(controller, {private_message, Nick, Recv, Msg}),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{name=Nick, next=chat}};
 
 handle_cast({chat, Nick, Str}, S) ->
@@ -63,43 +69,43 @@ handle_info({tcp, Socket, "!q"++_}, S = #state{name=Nick, next=chat}) ->
 
 handle_info({tcp, Socket, "!n"++_}, S = #state{name=Nick, next=chat}) ->
     gen_server:cast(self(), {nicklist, Nick}),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{name=Nick, next=chat}};
 
 handle_info({tcp, Socket, "!p:"++Rest}, S = #state{name=Nick, next=chat}) ->
     {Recv, [_|Msg]} = lists:splitwith(fun(T) -> [T] =/= ":" end, Rest),
     gen_server:cast(self(), {private_msg, Socket, Nick, Recv, clean(Msg)}),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{name=Nick, next=chat}};
 
 handle_info({tcp, Socket, "!h"++_}, S = #state{name=Nick, next=chat}) ->
     help(Socket, Nick),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{name=Nick, next=chat}};
 
 handle_info({tcp, _Socket, Str}, S = #state{socket=Socket, next=nick}) 
   when Str =:= "\n" ; Str =:= "\r\n" ->
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{socket=Socket, next=nick}};
 
 handle_info({tcp, _Socket, Str}, S = #state{socket=Socket, next=nick}) ->
     gen_server:cast(self(), {set_nick, Socket, clean(Str)}),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{socket=Socket, next=nick}};
 
 handle_info({tcp, _Socket, Str}, S = #state{socket=Socket, name=Nick, next=chat})
   when Str =:= "\n" ; Str =:= "\r\n" ->
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{name=Nick, next=chat}};
 
 handle_info({tcp, _Socket, "\\"++_Str}, S = #state{socket=Socket, name=Nick, next=chat}) ->
     io:format("We decided to keep this secret! ;-)~n", []),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S#state{name=Nick, next=chat}};
 
 handle_info({tcp, _Socket, Str}, S = #state{socket=Socket, name=Nick, next=chat}) ->
     gen_server:cast(self(), {chat, Nick, clean(Str)}), 
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     {noreply, S};
 
 handle_info({tcp_closed, _Socket}, S) ->
@@ -122,7 +128,8 @@ terminate(_Reason, _State) ->
     io:format("terminate reason: ~p~n", [_Reason]).
 
 %%%%%%%%%%%%%%%%%%%%%%
-% helpers
+%% helpers
+%%%%%%%%%%%%%%%%%%%%%%
 clean(Str) ->   
     hd(string:tokens(Str, "\r\n")).
 
@@ -136,6 +143,8 @@ help(Socket, _Nick) ->
 
 send(Socket, Str, Args) ->
     ok = gen_tcp:send(Socket, io_lib:format(Str++"~n", Args)),
-    ok = inet:setopts(Socket, [{active, once}]),
+    refresh_socket(Socket),
     ok.
 
+refresh_socket(Socket) ->
+    ok = inet:setopts(Socket, [{active, once}]).    
